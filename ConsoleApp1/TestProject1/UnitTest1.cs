3db51d52fc63d9c3836b89d7fc6877e291c4181e
vishwajeet <vishwajeet-pratap@cssoftsolutions.com>
2023-10-17 18:51:10 +0530

    code updated

---
+++ ConsoleApp1/TestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+
+namespace TestProject1 
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ConsoleApp1.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
